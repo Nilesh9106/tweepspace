e66660f68edfb5113d527383fa8d61b4d33b1279
Nilesh darji <nilesh.28.2003@gmail.com>
2024-05-05 13:36:25 +0530

    Some Fixes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,13 @@
 import { UsersHelper } from '@/helpers/users';
 import User from '@/models/user';
 import { UserTypeWithIds } from '@/types/model';
+import { dbConnect } from '@/utils/mongodb';
 import { MetadataRoute } from 'next';
 
 const URL = 'https://tweepspace.vercel.app';
 
 export default async function sitemap({ id }: { id: number }): Promise<MetadataRoute.Sitemap> {
+  await dbConnect();
   const users = await User.find();
   const routes = users.map(user => ({
     url: `${URL}/user/${user.username}`,
